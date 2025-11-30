4292bb0403a7025a688fc783de3c2088649597f7
Akriti1078 <[akritisok@gmail.com]>
2025-11-30 12:37:11 +0530

    Auto-update on 30-11-2025 12:37:11.71

--- auto_push.bat
+++ auto_push.bat
@@ -1,16 +1,24 @@
 @echo off
-echo Auto Git Push Started...
+cd "C:\IDEA Files\main\src"
+
+echo Initializing auto git push...
+git init
+
+REM Set GitHub remote repo
+git remote remove origin >nul 2>&1
+git remote add origin https://github.com/Akriti1078/Java-Programs.git
+
+git branch -M main
+
 :loop
-REM Add all changes
+echo Checking for changes...
+
 git add .
 
-REM Commit with timestamp
 git commit -m "Auto-update on %date% %time%" >nul 2>&1
 
-REM Push to GitHub
 git push origin main >nul 2>&1
 
-REM Wait for 30 seconds
 timeout /t 30 >nul
 
 goto loop
