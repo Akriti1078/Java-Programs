852b14e1ab888701a6e7f9bc9e5ab067ea2d046b
Akriti1078 <[akritisok@gmail.com]>
2026-02-11 12:54:26 +0530

    Auto-update on 11-02-2026 12:54:26.27

--- PracticePrograms/Calculator.java
+++ PracticePrograms/Calculator.java
@@ -1,4 +1,13 @@
 package PracticePrograms;
 
+java.util.Scanner;
+
 public class Calculator {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Calculator, What do you want to calculate: ")
+
+
+                
+    }
 }
