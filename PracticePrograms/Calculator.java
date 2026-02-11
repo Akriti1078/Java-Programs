6930a5b8435dfc0209cbc3d266ef4e134c6c0b61
Akriti1078 <[akritisok@gmail.com]>
2026-02-11 13:29:36 +0530

    Auto-update on 11-02-2026 13:29:36.18

--- PracticePrograms/Calculator.java
+++ PracticePrograms/Calculator.java
@@ -1,11 +1,26 @@
 package PracticePrograms;
 
+import com.sun.jdi.CharType;
+
+import java.util.Scanner;
+
 java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Calculator, What do you want to calculate: ")
+        System.out.println("Welcome to Calculator, What do you want to calculate: ");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input your First Number");
+        int a = sc.nextInt();
+
+
+        System.out.println("Select the operation you want to do: +, -, *, / ");
+        Char op = sc.next() +at()
+
+        System.out.println("Input Your Second Number");
+        int b = sc.nextInt();
 
 
 
